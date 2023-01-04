6f4f10f3bc469e2b89df2b0ee2b065e14159e723
kotani <21011103@c.anabuki-college.net>
2023-01-05 08:34:20 +0900

    vite

--- media/vite.config.js
+++ media/vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/index.scss', 'resources/js/article.js', 'resources/js/common.js'],
+            input: ['resources/sass/index.scss', 'resources/sass/common.scss',
+                'resources/sass/article.scss',
+                'resources/js/article.js', 'resources/js/common.js',
+                'resources/js/index.js',],
             refresh: true,
         }),
     ],
